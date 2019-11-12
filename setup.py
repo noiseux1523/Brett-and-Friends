f5c04fcfeaf366b19f67e4979355d62351d2804b
CedricNoiseux <cedric.noiseux@crim.ca>
2019-11-11 21:55:22 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Brett and Friends data exploration for homebrewing',
+    author='Brett-and-Friends',
+    license='',
+)
